8efb05af101dadb56ec0758251cc67568f87d217
72be890e <111967550+72be890e@users.noreply.github.com>
2023-07-27 22:41:28 +0200

    fix: alignment issues on mobile
    - Fix flex on host and tags
    - Design fixes on tags

--- src/pages/Housing/index.jsx
+++ src/pages/Housing/index.jsx
@@ -8,55 +8,58 @@ import Host from "../../components/Host/Host";
 import Rate from "../../components/Rate/Rate";
 
 function Housing() {
-  const { housingId } = useParams();
-  const navigate = useNavigate();
-  const housingData = data.find((i) => i.id === housingId);
+    const { housingId } = useParams();
+    const navigate = useNavigate();
+    const housingData = data.find((i) => i.id === housingId);
 
-  useEffect(() => {
-    if (!housingData) navigate("/not-found");
-  });
+    useEffect(() => {
+        if (!housingData) navigate("/not-found");
+    });
 
-  if (housingData) {
-    return (
-      <section className="page-container housing-container">
-        <Gallery images={housingData.pictures} />
+    if (housingData) {
+        return (
+            <section className='page-container housing-container'>
+                <Gallery images={housingData.pictures} />
 
-        <div className="info-container">
-          <div className="info-tags">
-            <div className="info-content">
-              <h1>{housingData.title}</h1>
-              <h2>{housingData.location}</h2>
-            </div>
-            <div className="info-tags-content">
-              {housingData.tags.map((tag) => (
-                <Tag key={`${housingData.id}-${tag}`} tag={tag} />
-              ))}
-            </div>
-          </div>
+                <div className='info-container'>
+                    <div className='info-tags'>
+                        <div className='info-content'>
+                            <h1>{housingData.title}</h1>
+                            <h2>{housingData.location}</h2>
+                        </div>
+                        <div className='info-tags-content'>
+                            {housingData.tags.map((tag) => (
+                                <Tag
+                                    key={`${housingData.id}-${tag}`}
+                                    tag={tag}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
 
-          <div className="info-stars-host-container">
-            <Host
-              hostName={housingData.host.name}
-              hostPicture={housingData.host.picture}
-            />
+                <div className='info-stars-host-container'>
+                    <Host
+                        hostName={housingData.host.name}
+                        hostPicture={housingData.host.picture}
+                    />
 
-            <Rate value={parseInt(housingData.rating)} />
-          </div>
-        </div>
+                    <Rate value={parseInt(housingData.rating)} />
+                </div>
 
-        <div className="housing-collapse-container">
-          <Collapse
-            description={`Description`}
-            content={housingData.description}
-          />
-          <Collapse
-            description={`Equipements`}
-            content={housingData.equipments}
-          />
-        </div>
-      </section>
-    );
-  }
+                <div className='housing-collapse-container'>
+                    <Collapse
+                        description={`Description`}
+                        content={housingData.description}
+                    />
+                    <Collapse
+                        description={`Equipements`}
+                        content={housingData.equipments}
+                    />
+                </div>
+            </section>
+        );
+    }
 }
 
 export default Housing;
